0c6f52c2472d37061b392e1082e1a32eca46feae
kmbolg09 <kmbolg09@gmail.com>
2014-03-31 12:03:12 -0700

    Update test.rb
    Commit this the master.

--- test.rb
+++ test.rb
@@ -1,3 +1,4 @@
  # This is a ruby file that should be merged with the master branch
  #add ruby code here for editing then pull back to the local
  #adding code here and then pushing it back to the master on to github to be worked on
+#what happens if I save this to another branch and try to pull it back?
