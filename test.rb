a8a05365a08fc9becfb5ca649fafb6281044959a
Karly Bolger <Karly@Karlys-MacBook-Pro.local>
2014-03-31 11:59:35 -0700

    Added some ruby comments

--- test.rb
+++ test.rb
@@ -1,2 +1,3 @@
  # This is a ruby file that should be merged with the master branch
  #add ruby code here for editing then pull back to the local
+ #adding code here and then pushing it back to the master on to github to be worked on
